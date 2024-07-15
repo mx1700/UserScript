70664c11436e528dbbf8bae217f59e88002d6ccf
zhangxx <mx1700@gmail.com>
2024-07-15 16:20:38 +0800

    Update meituan_down_imgs.user.js

--- meituan_down_imgs.user.js
+++ meituan_down_imgs.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://e.passiontec.cn/irs-web/gaia/index.html*
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      -
 // @description 2024/7/15 15:23:05
 // @grant        GM_download
